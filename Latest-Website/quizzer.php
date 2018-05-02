28817fd59085045c5bd3b1b547c95ea99a2b5f21
jellyaace <36072662+jellyaace@users.noreply.github.com>
2018-05-03 02:03:34 +0800

    Added quizzer.php

---
+++ Latest-Website/quizzer.php
@@ -0,0 +1,102 @@
+<?php
+	session_start();
+	include 'quizzer/database.php';
+	$database = new database;
+	$database->topic_show();
+?>
+
+<!DOCTYPE HTML>
+<!--
+-->
+<html>
+	<head>
+		<title>Webtech 2018</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="subpage">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="logo"><a href="index.html">Webtech <span>2018</span></a></div>
+				<a href="#menu">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="notes.html">Notes</a></li>
+					<li><a href="quizzer.html">Quizzer</a></li>
+				</ul>
+			</nav>
+
+		<!-- One -->
+			<section id="One" class="wrapper style3">
+				<div class="inner">
+					<header class="align-center">
+						<p>Test your knowledge</p>
+						<h2>My Quizzer</h2>
+					</header>
+				</div>
+			</section>
+
+		<!-- Main -->
+		
+				<div id="main" class="container">
+					<div class="inner">
+						<div class="grid-style">
+							
+								<div class="image fit">
+									<img src="images/image6.jpg" alt="" />
+								</div>
+									<div class="content">
+										<form method="post" action="quizzer/question.php" class="startquiz">
+											<select class="startquiz" id="" name="topic">
+											<option>Select Topic</option>
+											<?php
+												foreach($database->topic as $topic){
+												?>	
+												<option value="<?php echo $topic['topic_id'] ?>"><?php echo $topic['topic_name'] ?></option>
+												<?php	}
+											?>
+											</select><br>
+											<br>
+											<center><button type="submit" name="submit">Start Quiz</button></center>
+										</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+					
+							
+								
+
+					
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+					</ul>
+				</div>
+				<div class="copyright">
+					&copy; Untitled. All rights reserved.
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
