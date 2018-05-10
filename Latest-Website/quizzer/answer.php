e7946757220482e08902cbb9d05eaa1f53a4b7eb
jellyaace <2152940@slu.edu.ph>
2018-05-11 02:04:24 +0800

    modified some files

--- Latest-Website/quizzer/answer.php
+++ Latest-Website/quizzer/answer.php
@@ -52,16 +52,9 @@ $total_ans= $answer['right']+$answer['wrong']+$answer['no_answer'];
 		
 		<!-- Footer -->
 			<footer id="footer">
-				<div class="container">
-					<ul class="icons">
-						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
-					</ul>
-				</div>
+				
 				<div class="copyright">
-					&copy; Untitled. All rights reserved.
+					&copy; WebTech 9331A. All rights reserved.
 				</div>
 			</footer>
 			
